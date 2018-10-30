b201e2505268c62137afb470fefa28722c3d9bea
thewindsword_sfp4 <as5544070@gmail.com>
2018-10-30 10:29:57 +0800

    dev

--- src/bearyChatFunc.js
+++ src/bearyChatFunc.js
@@ -3,20 +3,32 @@ const bearychat = require('bearychat');
 const token = process.env.HUBOT_BEARYCHAT_TOKENS;
 
 module.exports = {
-    sendFile: (id)=>{
-        console.log('vchannel_id:'+id);
-        bearychat.message.create({
+    sendFile: async (id)=>{
+        // console.log('vchannel_id:'+id);
+        // bearychat.message.create({
+        //     token,
+        //     vchannel_id: id,
+        //     text: "中午吃啥啊",
+        //     attachments: [
+        //         {
+        //             color: "#ccc",
+        //             text: "中午吃什么？"
+        //         }
+        //     ]
+
+        // }).then(resp => resp.json())
+        // .then(data => console.log(data));
+        await bearychat.message.query({
             token,
             vchannel_id: id,
-            text: "中午吃啥啊",
-            attachments: [
-                {
-                    color: "#ccc",
-                    text: "中午吃什么？"
+            query: {
+                latest: {
+                    limit: 10,
                 }
-            ]
-
+            }
         }).then(resp => resp.json())
-        .then(data => console.log(data));
+        .then(data => {
+            return data;
+        });
     }    
 }
